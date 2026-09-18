093a3df5f8f4199e6375cb8e7b3b268350c32af9
Narwin K J <narwinkjpublic@gmail.com>
2026-09-18 09:27:36 +0530

    Fix formatting and minor syntax issues in palindrome function

--- code.py
+++ code.py
@@ -1,17 +1,17 @@
 def palindrome(number_received):
-    temp_number=number_received
-    number=0
+    temp_number = number_received
+    number = 0
     while temp_number > 0:
-        number=number*10+temp_number%10
-        temp_number//=10
-    if number_received==number:
+        number = number * 10 + temp_number % 10
+        temp_number //=1 0
+    if number_received == number:
         return "Palindrome"
     else:
         return "Non-Palindrome"
 while True:
-    string_received=input("Enter the number to check for Palindromicity : ")
+    string_received = input("Enter the number to check for Palindromicity : ")
     print(palindrome(int(string_received)))
-    if input("Enter yes to continue : ").lower().strip()=="yes":
+    if input("Enter yes to continue : ").lower().strip() == "yes":
         continue
     else:
         break
